e5bc6408aab55af8de5adf25714ab9a498a484f0
brunoveras19 <30731758+brunoveras19@users.noreply.github.com>
2024-07-07 13:46:01 -0300

    Adiciona sidebar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { intercept } from './interptors/auth.interceptor';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
 import { ProductService } from './services/auth/product.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(BrowserAnimationsModule),
     provideHttpClient(withInterceptors([intercept])),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
